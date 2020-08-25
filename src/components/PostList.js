b489eeb23ebb5425b9d18d0f8a861a2e00570e45
keremcanb <keremcan@gmail.com>
2020-08-26 00:30:07 +0300

    poslists

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
-const PostList = () => {
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-  return <div>Post List</div>;
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
 };
 
-export default connect(null, { fetchPosts: fetchPosts })(PostList);
-
-// class PostList extends React.Component {
-//   render() {
-//     return <div>Post List</div>;
-//   }
-// }
+export default connect(mapStateToProps, {
+  fetchPosts,
+})(PostList);
